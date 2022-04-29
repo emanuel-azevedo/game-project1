b0742b9eb629228d987b2c89ba389bb810417f26
Emanuel Azevedo <emanuelcansancao@hotmail.com>
2022-04-29 02:10:00 -0300

    Lógica implementada nos itens 3, 4, 5 e 6; Função resposta ok; Função alternativas retornando elementos repetidos, incompleta.

--- assests/script/rascunho.js
+++ assests/script/rascunho.js
@@ -1,15 +1,14 @@
 
 const imgList = []
 
-// 1. Esta função sortei um número aleatório entre 0 e comprimento da array passada como argumento:
+/*1. Esta função sortei um número aleatório entre 0 e comprimento da array passada como argumento: */
 function getRandomIndex (arr) {    
     let randomNum = Math.random() * arr.length;
     let randomIndex = Math.floor(randomNum);
     return randomIndex;
 }
 
-
-/*A função "compare" auxilia a segunda função "getSequenceWithoutRepeating" a
+/*2. A função "compare" auxilia a segunda função "getSequenceWithoutRepeating" a
 gerar uma sequência de exatamente 6 elementos aleatóriamente.*/
 
 function compare() {
@@ -17,8 +16,8 @@ function compare() {
 }
 
 function getSequenceWithoutRepeating () {
-  /* Array vazia declarada em variável
-  para obtenção futura da sequência */
+  /* Array vazia declarada em variável para
+  obtenção futura da sequência sem repetição*/
   let roundSequence = [];
   /* Reorganizo a array de clubes usando o método ".sort()"
   e passo como parâmetro a função "compare()"  */
@@ -34,25 +33,45 @@ function getSequenceWithoutRepeating () {
   return roundSequence
 }
 
-function getAlternative (roundSequence) {
-    let alternatives = [];
+/* 3. A const "displaySequence" guarda a array
+gerada pela função "getSequenceWithoutRepeating()"*/
+const displaySequence = getSequenceWithoutRepeating()
+
+/* 4. A função getAnswer obtém a resposta entre os 6 elementos gerados na função "getSequenceWithoutRepeating()" guardada na const "displaySequence" */
+function getAnswer(arr) {
+    let answerIndex = getRandomIndex(6)
+    return arr[answerIndex]  
+}
+
+/* 5. Guarda o valor (resposta) da função "getAnswer" na variável "answer" */
+const answer = getAnswer(displaySequence)
+
+/* 6. Cria as alternativas INCOMPLETA
+OBS: O retorno tem gerado arrays com repetição*/
+function getAlternatives () {
+ 
+    let alternatives = [];    
+    displaySequence.sort(compare)
     
-    for (let i = 0; i < 5; i++) {
-        let randomIndex = getRandomIndex(roundSequence);
-        alternatives.push(roundSequence[randomIndex])
-    }
+    for (let i = 0; i < 3; i++) {    
+      alternatives.push(displaySequence[i])    
+    }     
+    alternatives.splice(getRandomIndex(4), 0, answer)
+    // if (alternatives[i] = answer){
+    //   alternatives[i].splice(imgList[getRandomIndex(imgList.length)])
+    // }   
+     
+    return alternatives   
+}
 
-    return alternatives;    
-};
-
-function getRound () {
+/* function getRound () {
     let roundSequence = getSequence(imgList);
     let imgIndex = getRandomIndex(roundSequence);
-    let question = `Qual foi o ${imgIndex + 1}º clube apresentado na sequência?`
+    let question = `Qual foi o ${imgIndex + 1}º clube apresentado na sequência?`;
     let alternatives = getAlternative(roundSequence);
     let answer = roundSequence[imgIndex];
 
-};
+}
 
 function getCheck 
 
@@ -63,7 +82,7 @@ function getCheck
 
 //      5 7 10 11 16 18
 
-//      5 10 11 16 18
+//      5 10 11 16 18 */
 
 
 
