8916ec460a32dc4c0a7d08fa548d2a4148b70792
Emanuel Azevedo <emanuelcansancao@hotmail.com>
2022-04-30 12:19:17 -0300

    Criadas as funções question e alternatives (sem repetição)

--- assests/script/rascunho.js
+++ assests/script/rascunho.js
@@ -2,14 +2,12 @@
 const imgList = []
 
 /*1. Esta função sortei um número aleatório entre 0 e comprimento da array passada como argumento: */
-function getRandomIndex (arr) {    
-    let randomNum = Math.random() * arr.length;
-    let randomIndex = Math.floor(randomNum);
-    return randomIndex;
+function getRandomIndex(n) {    
+  return Math.floor(Math.random() * n);
 }
 
-/*2. A função "compare" auxilia a segunda função "getSequenceWithoutRepeating" a
-gerar uma sequência de exatamente 6 elementos aleatóriamente.*/
+/*2. A função "compare" auxilia a função seguinte "getSequenceWithoutRepeating" a
+gerar uma sequência de exatamente 6 elementos aleatoriamente.*/
 
 function compare() {
   return Math.random() - 0.5  
@@ -17,52 +15,60 @@ function compare() {
 
 function getSequenceWithoutRepeating () {
   /* Array vazia declarada em variável para
-  obtenção futura da sequência sem repetição*/
+  obtenção futura da sequência sem repetição. */
   let roundSequence = [];
-  /* Reorganizo a array de clubes usando o método ".sort()"
-  e passo como parâmetro a função "compare()"  */
+  /* Reorganiza a array de clubes usando o método ".sort()"
+  e passo como parâmetro a função "compare()". */
   imgList.sort(compare)
   /* Neste loop, a ideia é fazê-lo percorrer a array "imgList"
   já reorganizada pelo .sort(compare) e adicionar o índice onde estiver na
-  array declarada inicialmente até então vazia*/
-  for( let i = 0; i < 6; i++) {    
+  array declarada inicialmente, até então vazia. */
+  for (let i = 0; i < 6; i++) {    
     roundSequence.push(imgList[i])
   }
   /* Retorna a array declarada na variável
-  anterior com 6 elementos sortidos*/
+  anterior com 6 elementos sortidos. */
   return roundSequence
 }
 
 /* 3. A const "displaySequence" guarda a array
-gerada pela função "getSequenceWithoutRepeating()"*/
+gerada pela função "getSequenceWithoutRepeating()".*/
 const displaySequence = getSequenceWithoutRepeating()
 
-/* 4. A função getAnswer obtém a resposta entre os 6 elementos gerados na função "getSequenceWithoutRepeating()" guardada na const "displaySequence" */
-function getAnswer(arr) {
-    let answerIndex = getRandomIndex(6)
-    return arr[answerIndex]  
+// 4. Obtendo a questão charada do game
+// 4.1 "questionImgIndex" guarda um índice aleatório da Array  "displaySequence".
+const questionImgIndex = getRandomIndex(displaySequence.length)
+
+/* 4.2 A função "getQuestion" invoca a questão que o player deve responder escolhendo uma das alternativas que lhe serão ofertadas posteriormente */
+function getQuestion() {
+  /* 4.2.1 "questionImgIndex + 1" indica uma posição entre 1 e a posição do ultimo elemento da array "displaySequence". */
+  return`Qual foi o ${questionImgIndex + 1}º clube apresentado na sequência?`;  
 }
+// 4.3 A variável "question" guarda a pergunta gerada na função getQuestion.
+const question = getQuestion(.)
 
-/* 5. Guarda o valor (resposta) da função "getAnswer" na variável "answer" */
-const answer = getAnswer(displaySequence)
+/* 5. A variável "answer" salva o valor da resposta a partir da questão gerada pela função "getQuestion()". */
+const answer = displaySequence[questionImgIndex]
 
-/* 6. Cria as alternativas INCOMPLETA
-OBS: O retorno tem gerado arrays com repetição*/
-function getAlternatives () {
+/* 6. Cria 3 alternativas aleatórias entre as 6 da displaySequence em um array e adiciona a reposta "answer" em um índice aleatório. */
+function getAlternatives() {
  
-    let alternatives = [];    
-    displaySequence.sort(compare)
+  let alternatives = [];
+  // 6.1 Operador "spread: ..." => Cria "deep clones"
+  let displayNoAnswer = [...displaySequence]
+  let indexAnswer = displayNoAnswer.indexOf(answer)  
+  displayNoAnswer.splice(indexAnswer, 1)  
+  displayNoAnswer.sort(compare)
     
-    for (let i = 0; i < 3; i++) {    
-      alternatives.push(displaySequence[i])    
-    }     
-    alternatives.splice(getRandomIndex(4), 0, answer)
-    // if (alternatives[i] = answer){
-    //   alternatives[i].splice(imgList[getRandomIndex(imgList.length)])
-    // }   
-     
-    return alternatives   
+  for (let i = 0; i < 3; i++) {    
+    alternatives.push(displayNoAnswer[i])    
+  } 
+  alternatives.splice(getRandomIndex(4), 0, answer)
+  
+  return alternatives 
 }
+
+const alternatives = getAlternatives()
 
 /* function getRound () {
     let roundSequence = getSequence(imgList);
